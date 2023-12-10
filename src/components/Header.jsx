f457d8d37cca9665c28ce3dbbe31bfc24b7d2169
kevinvillajim <kevinvillajim@hotmail.com>
2023-12-09 22:59:14 -0500

    cambios extra cliente2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,25 @@
 import PropTypes from "prop-types";
 
-export function Header({ text }) {
-  return (
-    <>
-      <div className="sticky-container">
-        <a
-          href="/EcoPlagas/"
-          className="navbar-brand"
-        >
-          <img
-            className="logo"
-            alt="logo-eco-plagas"
-            src="https://raw.githubusercontent.com/kevinvillajim/EcoPlagas/main/public/logo2.png"
-          ></img>
-        </a>
-        <nav
-          id="cont-header"
-          className="navbar-fixed"
-        >
-          <div id="nav-text-container">{text}</div>
-        </nav>
-      </div>
-    </>
-  );
+export function Header({text}) {
+	return (
+		<>
+			<div className="sticky-container">
+				<a href="/EcoPlagas/" className="navbar-brand">
+					<img
+						className="logo"
+						alt="logo-eco-plagas"
+						src="https://raw.githubusercontent.com/kevinvillajim/EcoPlagas/main/public/logo2.png"
+					/>
+					<img className="logo2" alt="logo-eco-plagas2" src="/img/logo22.png" />
+				</a>
+				<nav id="cont-header" className="navbar-fixed">
+					<div id="nav-text-container">{text}</div>
+				</nav>
+			</div>
+		</>
+	);
 }
 
 Header.propTypes = {
-  text: PropTypes.element.isRequired,
+	text: PropTypes.element.isRequired,
 };
