da7b7a9d3c90f9266446a7bf578e9577b93568b9
kevinvillajim <kevinvillajim@hotmail.com>
2023-12-09 23:02:37 -0500

    cambios extra cliente2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,7 +10,11 @@ export function Header({text}) {
 						alt="logo-eco-plagas"
 						src="https://raw.githubusercontent.com/kevinvillajim/EcoPlagas/main/public/logo2.png"
 					/>
-					<img className="logo2" alt="logo-eco-plagas2" src="/img/logo22.png" />
+					<img
+						className="logo2"
+						alt="logo-eco-plagas2"
+						src="https://raw.githubusercontent.com/kevinvillajim/EcoPlagas/main/img/logo22.png"
+					/>
 				</a>
 				<nav id="cont-header" className="navbar-fixed">
 					<div id="nav-text-container">{text}</div>
